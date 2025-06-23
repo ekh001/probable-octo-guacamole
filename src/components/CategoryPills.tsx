b52219e6c9d50269562374ec58f595ca7cc74bb0
Liz Hall <lizk.hall@gmail.com>
2025-06-23 15:30:36 -0500

    Completes categories header

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,6 +1,6 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, ContainerIcon } from "lucide-react";
 import { Button } from "./Button";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 type CategoryPillProps = {
@@ -14,14 +14,31 @@ const TRANSLATE_AMOUNT = 200
 export function CategoryPills( { categories, selectedCategory, onSelect, }: CategoryPillProps) {
 
     const [translate, setTranslate] = useState(0)
+    const [isLeftVisible, setIsLeftVisible] = useState(false)
+    const [isRightVisible, setIsRightVisible] = useState(false)
+    const containerRef = useRef<HTMLDivElement>(null)
 
-    const [isLeftVisible, setIsLeftVisible] = useState(true)
-    const [isRightVisible, setIsRightVisible] = useState(true)
+    useEffect(() => {
+        if (containerRef.current == null) return
+        const observer = new ResizeObserver(entries => {
+            const container = entries[0]?.target
+            if (container == null) return
+
+            setIsLeftVisible(translate > 0)
+            setIsRightVisible(translate + container.clientWidth < container.scrollWidth)
+
+        })
+        observer.observe(containerRef.current)
+
+        return () => {
+            observer.disconnect()
+        }
+    }, [categories, translate])
 
     return (
     
 
-    <div className="overflow-x-hidden relative">
+    <div ref={containerRef} className="overflow-x-hidden relative">
         <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]" style={{ transform: `translateX(-${translate}px)`}}>
             {categories.map(category => (
             <Button
@@ -43,7 +60,6 @@ export function CategoryPills( { categories, selectedCategory, onSelect, }: Cate
                     const newTranslate = translate - TRANSLATE_AMOUNT
                     if (newTranslate <= 0) return 0
                     return newTranslate
-
                 })
             }}
             >
@@ -54,7 +70,22 @@ export function CategoryPills( { categories, selectedCategory, onSelect, }: Cate
         {isRightVisible && (
         <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
             <Button
-            variant="ghost" size="icon" className="h-full aspect-square w-auto p-1.5"
+            variant="ghost" size="icon" className="h-full aspect-square w-auto p-1.5" 
+            onClick={() => {
+                setTranslate(translate => {
+                    if (containerRef.current == null) {
+                        return translate
+                    }
+                    const newTranslate = translate + TRANSLATE_AMOUNT
+                    const edge = containerRef.current.scrollWidth
+                    const width = containerRef.current.clientWidth
+                    console.log(containerRef, translate)
+                    if (newTranslate + width >= edge) {
+                        return edge - width
+                    }
+                    return newTranslate
+                })
+            }}
             >
             <ChevronRight />
             </Button>
